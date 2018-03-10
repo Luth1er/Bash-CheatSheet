4dfd39de3b8f674011f97869fb8f78a4725985b0
[L]uth1er <xcultevil@gmail.com>
2018-03-10 18:50:59 -0500

    Update AuditingMailServers[NMAP].sh

--- AuditingMailServers[NMAP].sh
+++ AuditingMailServers[NMAP].sh
@@ -7,7 +7,7 @@ echo ""
 #echo -e "[!] Skip this step ? (y/N): "
 #sleep 0.5
 
-sudo nmap -sV --script smtp-open-relay -v 74.118.139.186
+sudo nmap -sV --script smtp-open-relay -v <target>
 
 
 echo "[!] Enumerating users in an SMTP server: "
@@ -18,7 +18,7 @@ echo "[!] Enumerating users in an SMTP server: "
 #Enumerating users via SMTP commands can obtain excellent results, 
 #and thanks to the Nmap Scripting Engine we can automate this task.
 
-sudo nmap -p25 –script smtp-enum-users 74.118.139.186
+sudo nmap -p25 –script smtp-enum-users <target>
 
 
 echo "[!] Detecting backdoor SMTP servers: "
@@ -28,7 +28,7 @@ echo "[!] Detecting backdoor SMTP servers: "
 
 
 
-sudo nmap -sV --script smtp-strangeport 74.118.139.186
+sudo nmap -sV --script smtp-strangeport <target>
 
 
 echo "[!] Brute forcing SMTP passwords: "
@@ -36,7 +36,7 @@ echo "[!] Brute forcing SMTP passwords: "
 #and penetration testers need to perform brute force password auditing against them to check for weak passwords.
 #This recipe will show you how to launch dictionary attacks against SMTP servers by using Nmap.
 
-sudo nmap -p 25 --script smtp-brute 74.118.139.186
+sudo nmap -p 25 --script smtp-brute <target>
 
 
 
@@ -45,14 +45,14 @@ echo "Retrieving the capabilities of a POP3 mail server"
 #By using a POP3 command we can list them, and thanks to Nmap, 
 #we can automate this task and include this service information in our scan results.
 
-sudo nmap -p110 --script pop3-capabilities 74.118.139.186
+sudo nmap -p110 --script pop3-capabilities <target>
 
 echo "[!] Brute forcing POP3 passwords: "
 #E-mail accounts store sensitive information. 
 #Penetration testers auditing mail servers must test for weak passwords that could help attackers compromise important accounts.
 #This recipe shows you how to perform brute force password auditing against POP3 mail servers by using Nmap.
 
-sudo nmap -p 110 --script pop3-brute 74.118.139.186
+sudo nmap -p 110 --script pop3-brute <target>
 
 
 echo "[!] Retrieving the capabilities of an IMAP mail server: "
@@ -61,14 +61,14 @@ echo "[!] Retrieving the capabilities of an IMAP mail server: "
 #mail server capabilities, and we can use Nmap to automate this task.
 
 
-sudo nmap -p143,993 --script imap-capabilities 74.118.139.186
+sudo nmap -p143,993 --script imap-capabilities <target>
 
 
 echo "[!] Brute forcing IMAP passwords: "
 #E-mail accounts store very sensitive information and penetration testers auditing a mail server must 
 #detect weak passwords that could compromise e-mail accounts and the information accessible through them.
 
-sudo nmap -p 143 --script imap-brute 74.118.186
+sudo nmap -p 143 --script imap-brute <target>
 
 
 
